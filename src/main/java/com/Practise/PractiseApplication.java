ad497f840f06ce6038ee26cfc1c42546ff655899
Gauravrmane <gauravmane44@gmail.com>
2024-11-26 10:20:13 +0530

    first test

---
+++ src/main/java/com/Practise/PractiseApplication.java
@@ -0,0 +1,14 @@
+package com.Practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseApplication {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello");
+		SpringApplication.run(PractiseApplication.class, args);
+	}
+
+}
